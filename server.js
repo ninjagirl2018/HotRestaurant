1391a936fbc21a91a445d9422052d6da10952ccb
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2019-11-28 21:08:14 -0500

    update 2

--- server.js
+++ server.js
@@ -4,13 +4,43 @@ const fs = require('fs');
 const app =  express();
 console.log('after calling readFile');
 
+
+
+let tables = [
+    {
+        reserved: false,
+        number: 3,
+    },
+    {
+        reserved: false,
+        number: 2,
+    },
+    {
+        reserved: false,
+        number: 1,
+    }
+]
+
+
 //ROUTES
 // Add two routes for the two pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+app.get('/api/tables', (req, res) => {
+    res.json(tables);
+  });
+
+
+// the code to process post requests
+app.post('/submit',function(request,response){
+    var query1=request.body.var1;
+    var query2=request.body.var2;
+    });
+
 //PORT
 const PORT = 8000 || process.env.PORT ;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
 
